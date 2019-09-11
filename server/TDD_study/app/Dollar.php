558f286d01d6288bbee120e2a2736b2cbfc89bb2
Tomoya Suzuki <forever.blue.moon@gmail.com>
2019-09-11 20:42:59 +0900

    第2章、「明白な実装」を完了

--- server/TDD_study/app/Dollar.php
+++ server/TDD_study/app/Dollar.php
@@ -12,9 +12,9 @@ class Dollar
 
     }
 
-    public function times(int $multiplier)
+    public function times(int $multiplier): Dollar
     {
-        $this->amount *= $multiplier;
+        return new Dollar($this->amount * $multiplier);
     }
 
 
--- server/TDD_study/tests/ToDoList.md
+++ server/TDD_study/tests/ToDoList.md
@@ -2,7 +2,7 @@
 
 ## MoneyTest
 - $5 + 10CHF = $10(レートが2:1の場合)
-- $5 * 2 = $10
+- OK $5 * 2 = $10
 - amountをprivateにする
-- Dollarの副作用どうする？
+- OK Dollarの副作用どうする？
 - Moneyの丸め処理どうする？
