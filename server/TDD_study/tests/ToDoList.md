612e59f9f2c728f6cdc4e5b947d1f617d27d305e
Tomoya Suzuki <forever.blue.moon@gmail.com>
2019-09-14 22:54:17 +0900

    第5章「あえて原則を破る」実装完了。Dollarと同様の動作を持つFrancクラスを実装したが、重複が過分に含まれている。まさにコピペプログラマーの所業だが、Red→Greenの操作に於いては速度が何よりも重要視される。Refactorで重複を排除すればよい。

--- server/TDD_study/tests/ToDoList.md
+++ server/TDD_study/tests/ToDoList.md
@@ -10,3 +10,7 @@
 - hashCode()
 - nullとの等価性比較
 - 他のオブジェクトとの等価性比較
+- OK 5CHF * 2 = 10 CHF
+- DollarとFrancの重複
+- equalsの一般化
+- timesの一般化
--- server/TDD_study/tests/Unit/MoneyTest.php
+++ server/TDD_study/tests/Unit/MoneyTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Dollar;
+use App\Franc;
 
 class MoneyTest extends TestCase
 {
@@ -21,4 +22,18 @@ class MoneyTest extends TestCase
         $this->assertTrue($dollar->equals(new Dollar(5)));
         $this->assertFalse($dollar->equals(new Dollar(6)));
     }
+
+    public function testFrancMultiplication()
+    {
+        $five = new Franc(5);
+        $this->assertEquals(new Franc(10),$five->times(2));
+        $this->assertEquals(new Franc(15),$five->times(3));
+    }
+
+//    public function testFrancEquality()
+//    {
+//        $dollar = new Franc(5);
+//        $this->assertTrue($dollar->equals(new Franc(5)));
+//        $this->assertFalse($dollar->equals(new Franc(6)));
+//    }
 }
